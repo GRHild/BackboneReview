7150c0c8e14b2b46a8071a638911a4870d425b42
Greg Hild <grhild@gmail.com>
2013-10-07 11:33:25 -0500

    Add +1 quantity to type

--- src/views/inventory-view.js
+++ src/views/inventory-view.js
@@ -12,6 +12,19 @@
       // add +1 to each of that shirt's `type` inventory.
       // For example, if type === 'small', then add +1 to every small stock
       // in each shirt in window.shirts. Use .set(???) for this.
+
+      var shirts = this.options.shirts;
+
+      for (var i = 0; i < shirts.length; i += 1) {
+        // console.log('length of shirts array', shirts.length);
+        // console.log('what is type', type);
+        // console.log('shirts', shirts);
+        // console.log('shirts[i]', shirts[i]);
+        // console.log("shirts[i].get(type)", shirts[i].get(type));
+        quantity = shirts[i].get(type);
+        shirts[i].set(type, quantity += 1);
+        // console.log("shirts[i].get(type)", shirts[i].get(type));
+      }
     }
   });
 
